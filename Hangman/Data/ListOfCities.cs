1251861889562a7ee59ab484ebae8a45e052e96b
mihuwis <mihuwis@gmail.com>
2023-10-10 15:12:58 +0200

    Add list of cities in Data Module

---
+++ Hangman/Data/ListOfCities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Data
+{
+    internal class ListOfCities
+    {
+        private List<string> cities;
+
+        public ListOfCities()
+        {
+            cities = new List<string>();
+            cities.Add("Warsaw");
+            cities.Add("Prag");
+            cities.Add("Kabul");
+            cities.Add("London");
+        }
+    }
+}
