2a4678e63b8c76daaaad823f0845e404fd2b90fa
mihuwis <mihuwis@gmail.com>
2023-10-12 17:03:04 +0200

    Add game contoller

--- Hangman/Controler/GameController.cs
+++ Hangman/Controler/GameController.cs
@@ -1,4 +1,5 @@
-﻿using Hangman.View;
+﻿using Hangman.Model;
+using Hangman.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,16 @@ namespace Hangman.Controler
 {
     internal class GameController
     {
-        GameMessages gameMesages = new GameMessages();
-        InputController inputController = new InputController();
+        GameMessages gameMesages = new();
+        CitiesGenerator citiesGenerator = new();
+        ListOfCities listOfCities = new();
+        InputController inputController = new();
 
         public void RunTheGame()
         {
             Console.WriteLine(gameMesages.WelcomeMsg);
+            string capitolToGuess = citiesGenerator.generateCity(listOfCities.Cities);
+            Console.WriteLine(capitolToGuess);
             Console.WriteLine(gameMesages.EnterLetterMsg);
             string letter = inputController.getInputValueFromUser();
   
